559ac2fdd0305ce4eba8549306c95cad8beec6b8
Alex <speedy_sky@mail.ru>
2021-04-01 18:30:51 +0300

    clearing

--- object_follower/src/object_follower/GoalPublisherTf.cpp
+++
@@ -1,6 +0,0 @@
-#include "GoalPublisherTf.hpp"
-
-void GoalPublisherTf::sendGoal(geometry_msgs::TransformStamped &pose) {
-  pose.child_frame_id = goal_frame_;
-  tf_broadcaster_.sendTransform(pose);
-}
